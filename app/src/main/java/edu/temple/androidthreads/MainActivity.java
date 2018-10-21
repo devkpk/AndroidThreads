70ca9e20a2e811c4b79063abe14348ccbf606898
karl <karl@macc.io>
2018-10-21 16:26:52 -0400

    Added spinlock for pause/unpause.

--- app/src/main/java/edu/temple/androidthreads/MainActivity.java
+++ app/src/main/java/edu/temple/androidthreads/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends Activity {
 
     int countdownTimer = 10;
 
+    volatile int state; // 0 - Not started, 1 - Running, 2 - Paused
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,46 +23,59 @@ public class MainActivity extends Activity {
 
         timerTextView = (TextView) findViewById(R.id.timerTextView);
 
+        timerTextView.setText(String.valueOf(countdownTimer));
+
         findViewById(R.id.startButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                timerTextView.setText(String.valueOf(countdownTimer));
+                if (state == 0) {
+                    state = 1;
+                    // Thread to run background operation
+                    Thread t = new Thread() {
+                        @Override
+                        public void run() {
+                            for (int i = countdownTimer; i >= 0; i--) {
 
-                // Thread to run background operation
-                Thread t = new Thread(){
-                    @Override
-                    public void run(){
-                        for (int i = countdownTimer; i >= 0; i--){
+                                // The line below throws an exception. You cannot interact with the
+                                // UI thread from a worker thread
+                                /* timerTextView.setText(String.valueOf(i)); */
 
-                            // The line below throws an exception. You cannot interact with the
-                            // UI thread from a worker thread
-                            /* timerTextView.setText(String.valueOf(i)); */
+                                // Obtain a message from the global message pool
+                                Message msg = timerHandler.obtainMessage();
 
-                            // Obtain a message from the global message pool
-                            Message msg = timerHandler.obtainMessage();
+                                // Assign a value to the 'what' variable. Could have also used
+                                // overloaded obtainMessage(int what) method
+                                msg.what = i;
 
-                            // Assign a value to the 'what' variable. Could have also used
-                            // overloaded obtainMessage(int what) method
-                            msg.what = i;
+                                // Send message to the handler
+                                timerHandler.sendMessage(msg);
 
-                            // Send message to the handler
-                            timerHandler.sendMessage(msg);
+                                try {
+                                    // Sleep for 1 second
+                                    Thread.sleep(1000);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
 
-                            try {
-                                // Sleep for 1 second
-                                Thread.sleep(1000);
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
+                                // Spinlock to pause timer
+                                //noinspection StatementWithEmptyBody
+                                while (state == 2);
+
+                                Log.d("Thread timer value", String.valueOf(i));
                             }
 
-                            Log.d("Thread timer value", String.valueOf(i));
+                            state = 0;
                         }
-                    }
-                };
+                    };
 
-                // Start the thread
-                t.start();
+                    // Start the thread
+                    t.start();
+                } else if (state == 1) {
+                    state = 2; // paused
+                } else {
+                    state = 1;
+                }
             }
         });
     }
